78086d37299c8f306197de12782a44c5d7230526
François Schmidts <francois.schmidts@dolead.com>
2014-01-29 18:12:38 +0100

    overriding update, it must not be aware of soft deletion

--- extras_mongoengine/document.py
+++ extras_mongoengine/document.py
@@ -1,5 +1,5 @@
 from mongoengine.base import TopLevelDocumentMetaclass
-from mongoengine.queryset import QuerySet
+from mongoengine.queryset import QuerySet, OperationError
 from mongoengine.document import Document
 
 
@@ -21,14 +21,20 @@ class SoftDeleteQuerySet(QuerySet):
 
     def __call__(self, q_obj=None, class_check=True, slave_okay=False,
             read_preference=None, **query):
+        """A simple wrapper around ~mongoengine.queryset.QuerySet.__call__ that
+        allows query parameters to override those written in the initial query.
+        """
         for key in set(query).intersection(self._document._meta['soft_delete']):
             del self._initial_query[key]
         return super(SoftDeleteQuerySet, self).__call__(q_obj=None,
                 class_check=True, slave_okay=False, read_preference=None,
                 **query)
 
+    @property
     def including_soft_deleted(self):
-        for key in self._document._meta['soft_delete']:
+        """Will clean the queryset from soft_delete notions."""
+        for key in set(self._initial_query)\
+                .intersection(self._document._meta['soft_delete']):
             del self._initial_query[key]
         return self.clone()
 
@@ -40,7 +46,7 @@ class SoftDeleteDocument(Document):
     meta = {'queryset_class': SoftDeleteQuerySet}
 
     @property
-    def _qs(self):  #FIXME
+    def _qs(self):  #FIXME should be present in mongoengine ?
         """
         Returns the queryset to use for updating / reloading / deletions
         """
@@ -50,9 +56,12 @@ class SoftDeleteDocument(Document):
         return self.__objects
 
     def soft_delete(self):
+        """Won't delete the document as much as marking it as deleted according
+        to parameters present in meta.
+        """
         soft_delete = self._meta.get('soft_delete')
         cls_name = self.__class__.__name__
-        assert soft_delete, "%s's meata doesn't exist" % cls_name
+        assert soft_delete, "%s's meta doesn't exist" % cls_name
         assert type(soft_delete) is dict, \
                 "%s's soft_delete must be a dictionary" % cls_name
         for key in soft_delete:
@@ -60,3 +69,20 @@ class SoftDeleteDocument(Document):
                     "%s's meta has no field %s" % (cls_name, key)
             setattr(self, key, soft_delete[key])
         self.save()
+
+    def update(self, **kwargs):
+        """The ~mongoengine.Document.update method had to be overriden
+        so it's not soft_delete aware and will update document
+        no matter the "soft delete" state.
+        """
+        if not self.pk:
+            if kwargs.get('upsert', False):
+                query = self.to_mongo()
+                if "_cls" in query:
+                    del(query["_cls"])
+                return self._qs.including_soft_deleted\
+                        .filter(**query).update_one(**kwargs)
+            else:
+                raise OperationError('attempt to update a document not yet saved')
+        return self._qs.including_soft_deleted\
+                .filter(**self._object_key).update_one(**kwargs)
